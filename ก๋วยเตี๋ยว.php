e42f44c076b40cf32a77947eeaa8c06bdd2c2ca9
thongthogn <thongchai-2534@hotmail.co.th>
2014-10-16 00:20:38 +0700

    Noodles

---
+++ ก๋วยเตี๋ยว.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form action="" method="post" enctype="multipart/form-data" name="form1" id="form1">
+  <table width="563" border="1">
+    <tr>
+      <td width="553"\t><center>ก๋วยเตี๋ยวต้มยำบ้านลายจุดข้างวัง</center></td>
+    </tr>
+    <tr>
+      <td><center>
+      ราคาเริ่มต้น 35 บาท ใน35บาท จะมีหมู ลูกชิ้น หมูสับ ตับลวก
+      </center></td>
+    </tr>
+  </table>
+  <br />
+  <p>เลือกเส้น
+    <label>
+      <input type="checkbox" name="CheckboxGroup1" value="checkbox" id="CheckboxGroup1_0" />
+      เส้นเล็ก</label>
+
+    <label>
+      <input type="checkbox" name="CheckboxGroup1" value="checkbox" id="CheckboxGroup1_1" />
+      เส้นหมี่</label>
+   
+    <label>
+      <input type="checkbox" name="CheckboxGroup1" value="checkbox" id="CheckboxGroup1_2" />
+      บะหมี่</label>
+   
+    <label>
+      <input type="checkbox" name="CheckboxGroup1" value="checkbox" id="CheckboxGroup1_3" />
+      วุ้นเส้น </label>
+    ราคา35บ.
+  </p>
+  <p>น้ำซุบ
+    <label>
+      <input type="checkbox" name="CheckboxGroup2" value="checkbox" id="CheckboxGroup2_0" />
+    น้ำตก</label>
+    <label>
+      <input type="checkbox" name="CheckboxGroup2" value="checkbox" id="CheckboxGroup2_1" />
+      น้ำใส่</label>
+  </p>
+  <p>เพิ่มเติม
+    <label>
+      <input type="checkbox" name="CheckboxGroup3" value="checkbox" id="CheckboxGroup3_0" />
+      ไข่มะตูมราคาล่ะ15</label>
+    บ.
+    
+    <label>
+      <input type="checkbox" name="CheckboxGroup3" value="checkbox" id="CheckboxGroup3_1" />
+      กุ้งราคาตัวล่ะ55บ.</label>
+    <label>
+      <input type="checkbox" name="CheckboxGroup3" value="checkbox" id="CheckboxGroup3_2" />
+      ปลาหมึกราคาตัวล่ะ55บ.</label>
+    <br />
+    <br />
+    จำนวน
+    <label for="textfield"></label>
+    <input type="text" name="textfield" id="textfield" />
+  ชาม</p>
+  <p>
+    <input type="submit" name="button" id="button" value="คำนวน" />
+    <br />
+    <label for="textfield2"></label>
+    <input type="text" name="textfield2" id="textfield2" />
+  </p>
+  <p>
+    <input type="submit" name="button2" id="button2" value="ส่ง" />
+    <input type="submit" name="button3" id="button3" value="แก้ไข" />
+    <br />
+  </p>
+  <p><br />
+  </p>
+  <p><br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  </p>
+  <p>&nbsp;</p>
+</form>
+</body>
+</html>
